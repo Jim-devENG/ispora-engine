e01d136fdbe387ecb02f94b5b2fd111c6bd03354
Crys Garage Developer <developer@crysgarage.com>
2025-10-02 20:16:51 +0100

    Fix AccountSwitcher TooltipTrigger ref warning
    - Add asChild prop to TooltipTrigger wrapping TriggerButton component
    - TriggerButton properly forwards refs with React.forwardRef

--- iSpora-frontend/components/AccountSwitcher.tsx
+++ iSpora-frontend/components/AccountSwitcher.tsx
@@ -56,7 +56,7 @@ export function AccountSwitcher({ trigger, showInHeader = false }: AccountSwitch
 
   const defaultTrigger = showInHeader ? (
     <Tooltip>
-      <TooltipTrigger>
+      <TooltipTrigger asChild>
         <TriggerButton />
       </TooltipTrigger>
       <TooltipContent side="bottom">
